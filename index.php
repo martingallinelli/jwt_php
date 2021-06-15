2407faea22d548e747a155051c099eeb38fc7b4c
martin gallinelli <martiin.edlp@gmail.com>
2021-06-15 15:51:11 -0300

    update title

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if (isset($_GET['p'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cursos</title>
+    <title>JWT</title>
 
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
